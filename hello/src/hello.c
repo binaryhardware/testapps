fe12fbb3c39395bbd2d9712fbef19dde0cff7433
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-11-30 18:25:09 +0330

    NewTest: Hello world app

---
+++ hello/src/hello.c
@@ -0,0 +1,16 @@
+/* main.c - hello app
+  created by @alimpk
+
+  Copyright (C) 2019-2020 Binary Hardware Group.
+  This file is part of Testapps, Binary Hardware Group Toolchains test sources.
+  You should have received a copy of the GNU General Public License along with
+  Testapps;
+
+*/
+#include "stdio.h"
+
+int main(int argc, char const *argv[]) {
+
+  printf("hello, BHG\n");
+  return 0;
+}
